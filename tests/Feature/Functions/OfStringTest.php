bfe4a45fc7b7e681650c2eba210c6a05ad78818b
Prince John Santillan <mail@princejohnsantillan.net>
2025-10-18 10:53:11 +0800

    updates

--- tests/Feature/Functions/OfStringTest.php
+++ tests/Feature/Functions/OfStringTest.php
@@ -11,11 +11,3 @@ it('generates a string from the parameter when none is provided', function () {
     $of = weave('{{:of,Hello}}');
     expect($of)->toBe('Hello');
 });
-
-it('passes thru the given string', function () {
-    $of = weave('{{:of,Hello}}', ['Hello']);
-    expect($of)->toBe('Hello');
-
-    $of = weave('{{:of}}', ['World']);
-    expect($of)->toBe('World');
-});
