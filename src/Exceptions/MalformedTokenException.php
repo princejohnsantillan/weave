a69ece914b1463fab2239937291743a7949b55a0
Prince John Santillan <mail@princejohnsantillan.net>
2025-10-21 18:39:26 +0800

    Improve blank token support

--- src/Exceptions/MalformedTokenException.php
+++ src/Exceptions/MalformedTokenException.php
@@ -4,12 +4,4 @@ namespace PrinceJohn\Weave\Exceptions;
 
 use Exception;
 
-final class MalformedTokenException extends Exception
-{
-    public const BLANK_TOKEN = 1000;
-
-    public static function blankToken(): MalformedTokenException
-    {
-        return new MalformedTokenException('A token cannot be blank.', MalformedTokenException::BLANK_TOKEN);
-    }
-}
+final class MalformedTokenException extends Exception {}
